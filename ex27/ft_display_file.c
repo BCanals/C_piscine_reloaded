7b9aecec5474b0f46ab9fa3d1e2e7383fbf71819
Bernat Canals I Guàrdia <bcanals-@cbr10s6.42barcelona.com>
2024-06-25 20:18:01 +0200

    final update, moulinette-validated code :)

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,16 +6,54 @@
 /*   By: bcanals- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 16:22:49 by bcanals-          #+#    #+#             */
-/*   Updated: 2024/06/14 16:39:49 by bcanals-         ###   ########.fr       */
+/*   Updated: 2024/06/25 19:19:38 by bcanals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(int argc, int **argv)
+void	ft_putchar(char c, int std)
+{
+	write(std, &c, 1);
+}
+
+void	ft_putstr(char *str, int std)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++], std);
+}
+
+void	show_char(char *argv)
+{
+	char	talkback;
+	int		fd;
+	int		eof;
+
+	fd = open(argv, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr("Cannot read file.\n", 2);
+		return ;
+	}
+	eof = read(fd, &talkback, 1);
+	while (eof > 0)
+	{
+		ft_putchar(talkback, 1);
+		eof = read(fd, &talkback, 1);
+	}
+	close(fd);
+}
+
+int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		fprintf(stderr, "File name missing.\n");
+		ft_putstr("File name missing.\n", 2);
 	else if (argc > 2)
-		fprintf(stderr, "Too many arguments.\n");
+		ft_putstr("Too many arguments.\n", 2);
+	else
+		show_char(argv[1]);
 }
